29ec20d11c8f83db2aa405790f58744011f4f1f3
PAThological101 <44306065+PAThological101@users.noreply.github.com>
2023-11-14 04:52:05 -0500

    Update appYin.py

--- appYin.py
+++ appYin.py
@@ -989,4 +989,4 @@ def update_charts(selected_reviewer):
 ###############################################################################################################################################################
 
 if __name__ == '__main__':
-  app.run_server(host='0.0.0.0', port=8050, debug=False)
+  app.run_server(debug=True, port=os.environ.get('PORT', 8080))#for aws app runner
